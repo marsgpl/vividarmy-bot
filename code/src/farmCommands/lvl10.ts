dbb6c34550bcc9e6703ffa311de7ff041e337aa4
marsgpl <marsgpl@gmail.com>
2020-10-16 14:51:49 +0300

    kek

--- code/src/farmCommands/lvl10.ts
+++ code/src/farmCommands/lvl10.ts
@@ -54,6 +54,7 @@ export default async function(this: Farm): Promise<void> {
     await puppet.researchScienceById(320007, 'Army units 6->7');
     await puppet.researchScienceById(320008, 'Army units 7->8');
     await puppet.repairBuildingsByTypeId(2101, 'Repair Factory');
+    await puppet.claimEventReward({ aid:37, tid: 1139 });
 
     // receive unit from npc
     // {"c":1152,"o":"140","p":{"multiple":0}}
