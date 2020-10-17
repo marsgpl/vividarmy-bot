b04644d4f4829689a4f6e2a016d84391b450547a
marsgpl <marsgpl@gmail.com>
2020-10-18 02:17:38 +0300

    kek

--- code/src/farmCommands/reinforceCapitalBkp.ts
+++ code/src/farmCommands/reinforceCapitalBkp.ts
@@ -4,7 +4,6 @@ export default async function(this: Farm): Promise<void> {
     const puppetId = process.argv[3];
     const puppet = await this.getPuppetById(puppetId);
 
-    await puppet.reinforceCapitalWithSingleUnit(99999);
     await puppet.useMarchQueuePlus1();
     await puppet.reinforceCapitalWithSingleUnit(10006);
 }
